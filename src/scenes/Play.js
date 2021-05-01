5a4b28e41631c8a19524d8d7695bed078c1134d6
laadiaz <laadiaz@ucsc.edu>
2021-04-30 17:14:35 -0700

    comments

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -112,7 +112,7 @@ class Play extends Phaser.Scene {
         // GAME OVER flag
         this.gameOver = false;
 
-        // 60-second play clock
+        //play clock
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
