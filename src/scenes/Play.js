e8ad3ae528bc43333776785541bf010495b5f447
laadiaz <laadiaz@ucsc.edu>
2021-05-05 23:39:18 -0700

    basic collsion logic and hitbox fixes and scoring mechanic

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -22,10 +22,13 @@ class Play extends Phaser.Scene {
         this.ACCELERATION = 200;
         this.DRAG = 200;    // DRAG < ACCELERATION = icy slide
         this.physics.world.gravity.y = 200;
-
+        
+        this.death_flag = false;
         this.jumped = false;
         this.apex = false;
         this.moveSpeed = fallspeed; //pixels per frame
+
+        this.fall_distance = 0;
 
         //place well background
         this.well = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'well').setOrigin(0, 0);
@@ -46,35 +49,11 @@ class Play extends Phaser.Scene {
         cursors = this.input.keyboard.createCursorKeys();
         
         /* create player and declare sprite */
-        this.guy = this.physics.add.sprite(240, game.config.height - 550, 'sprite_atlas', 'fall01').setScale();
+        this.guy = this.physics.add.sprite(240, game.config.height - 512, 'sprite_atlas', 'fall01').setScale();
         this.guy.setCollideWorldBounds(true);
         this.guy.setMaxVelocity(100, 1000000);
-
+        this.guy.setSize(30, 60, true);
         this.physics.add.collider(this.guy, this.ground);
-
-        /* Create branch and place in scene */
-        this.branch01 = this.physics.add.sprite(400, game.config.height + 500, 'sprite_atlas', 'branch_01').setScale();
-        this.branch01.setCollideWorldBounds(false);
-        this.branch01.body.allowGravity = false;
-        this.branch01.setVelocityY(-this.moveSpeed * 60);
-        this.branch01.setMaxVelocity(0, 1000000);
-        
-
-        //create 2nd branch on the left side of the screen 
-
-        /*
-        this.branch02 = this.physics.add.sprite(80, game.config.height + 500, 'sprite_atlas', 'branch_01').setScale();
-        this.branch02.setCollideWorldBounds(false);
-        this.branch02.body.allowGravity = false;
-        this.branch02.setVelocityY(-this.moveSpeed * 60);
-        this.branch02.setMaxVelocity(0, 1000000);
-        this.branch02.setFlip(true, false);
-        */
-
-        //this.branch = new Branch(this, 1);
-
-        this.physics.add.collider(this.guy, this.branch01);
-
 
         //Set up Animations - note: 'fall' is the "idle" animation
         this.anims.create({
@@ -139,11 +118,14 @@ class Play extends Phaser.Scene {
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
+
+        this.fallRight = this.add.text(game.config.width - borderUISize - borderPadding - 120, borderUISize + borderPadding*2, this.fall_distance + "m", scoreConfig);
     }
 
     addBranch() {
         let invert =  Phaser.Math.Between(0, 1);
         let branch = new Branch(this, invert);
+        branch.setSize(130, 5, true);
         this.physics.add.collider(this.guy, branch);
         this.branchGroup.add(branch);
     }
@@ -158,7 +140,11 @@ class Play extends Phaser.Scene {
 
     update() {
 
-        //console.log ("AAAAHHH");
+
+        this.fall_distance = Math.trunc(8 * this.clock.getElapsedSeconds() + this.moveSpeed);
+
+        this.fallRight.text = this.fall_distance + "m";
+
          // check keyboard input
          if(cursors.left.isDown) {
             this.guy.body.setAccelerationX(-this.ACCELERATION);
@@ -174,25 +160,15 @@ class Play extends Phaser.Scene {
             this.guy.body.setAccelerationX(0);
             this.guy.body.setDragX(this.DRAG);
         }
-
-        /*if(this.alien.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.space)) {
-            this.alien.body.velocity.y = this.JUMP_VELOCITY;
-        } else {
-            this.alien.body.setMaxSpeed(100);
-        }
-        
-        console.log(this.alien.velocity)
-        */
-        
-        
+            
         if (!this.jumped && !this.apex) {
             this.well.tilePositionY += this.moveSpeed;
-            this.branch01.setVelocityY(-this.moveSpeed * 60);       //branch velocity updated every frame
+            // this.branch01.setVelocityY(-this.moveSpeed * 60);       //branch velocity updated every frame
         } else if (this.jumped && !this.apex) {
                                        //jump anim
             this.well.tilePositionY += this.moveSpeed;
             this.moveSpeed -= (fallspeed / 10);
-            this.branch01.setVelocityY(-this.moveSpeed * 60);
+            // this.branch01.setVelocityY(-this.moveSpeed * 60);
             //console.log (this.moveSpeed);
             if (this.moveSpeed < (fallspeed * -1.5)) {
                 this.apex = true;
@@ -203,7 +179,7 @@ class Play extends Phaser.Scene {
             this.well.tilePositionY += this.moveSpeed;
             if (this.moveSpeed < fallspeed) {
                 this.moveSpeed += (fallspeed / 10);
-                this.branch01.setVelocityY(-this.moveSpeed * 60);
+                // this.branch01.setVelocityY(-this.moveSpeed * 60);
             } else {
                 this.jumped = false;
                 this.apex = false;
@@ -215,10 +191,27 @@ class Play extends Phaser.Scene {
             this.guy.anims.play('jump'); 
             this.jumped = true;
         }
-        //this.well.tilePositionY += this.alien.body.velocity.y / 10;
+        if (!this.death_flag) {
+            this.physics.overlap(this.guy, this.branchGroup, this.death, null, this);
+        }
 
-        // wrap physics object(s) .wrap(gameObject, padding)
-        //this.physics.world.wrap(this.branch01);
+    }
+
+    death() {
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'dead', scoreConfig).setOrigin(0.5);
+        this.death_flag = true;
 
     }
 }
