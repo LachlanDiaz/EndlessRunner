2d5770f9d3f622629cfdcb0e84d80baf0ca61016
laadiaz <laadiaz@ucsc.edu>
2021-05-07 15:52:23 -0700

    menu navigation is implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,6 +8,8 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.image('main_menu', './assets/main_menu.png');
+        this.load.image('pointer', './assets/pointer.png');
       }
 
     create() {
@@ -23,6 +25,16 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
+
+
+
+        this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'main_menu').setOrigin(0, 0);
+
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.menu_option = 1;
+
+        this.add_pointer();
 
         //show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'FALLER 0.1', menuConfig).setOrigin(0.5);
@@ -44,6 +56,39 @@ class Menu extends Phaser.Scene {
             }
             this.sound.play('sfx_select');
             this.scene.start('playScene');    
+        }
+
+
+        if (Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            this.pointer.destroy();
+            this.menu_option--;
+            if (this.menu_option > 3) {
+                this.menu_option = 1;
+            } else if (this.menu_option < 1) {
+                this.menu_option = 3;
+            }
+            this.sound.play('sfx_select');
+            this.add_pointer();
+        } else if (Phaser.Input.Keyboard.JustDown(cursors.down)) {
+            this.pointer.destroy();
+            this.menu_option++;
+            if (this.menu_option > 3) {
+                this.menu_option = 1;
+            } else if (this.menu_option < 1) {
+                this.menu_option = 3;
+            }
+            this.sound.play('sfx_select');
+            this.add_pointer();
+        }
+    }
+    add_pointer() {
+        if (this.menu_option == 1) {
+            this.pointer = this.add.image(80, 290, 'pointer');
+
+        } else if (this.menu_option == 2) {
+            this.pointer = this.add.image(80, 378, 'pointer');
+        } else if (this.menu_option == 3) {
+           this.pointer = this.add.image(100, 500, 'pointer');
         }
     }
 }
