b50d32b005686af440373946f06f6ff40ddd7d2f
laadiaz <laadiaz@ucsc.edu>
2021-05-08 12:05:26 -0700

    added main comment block

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+/*
+Technically Interesting:
+For something technical we did, was that we implemented a full menu
+navigation into the main menu and game over screens. We also created
+our own physics of branch and player movement for the jump, which was
+a bit difficult (and still a bit wonky). We also made a tutorial that
+pauses the current scene and manages multiple scenes at once. Text 
+bubbles were also implemenmted. 
+
+Visual Style:
+We are very pround of our visual style, particualrly due to the fact 
+that we have two different levels depending on if the player is
+playing the tutorial or not. The music is bumpin too.
+*/
+
+
+
 // game configureation
 let config = {
     parent: 'myGame',
